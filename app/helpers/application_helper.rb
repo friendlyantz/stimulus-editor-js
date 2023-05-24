9c9a879aca521b06c2caf6c56d1d6ef0964a6ac7
Anton Panteleev <friendlyantz@pm.me>
2023-05-24 21:17:13 +1000

    Hide EditorJS backend code from the content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def display_content(content)
+    parsed_content = JSON.parse(content)
+    content_html = parsed_content["blocks"].map do |block|
+      case block["type"]
+      when "paragraph"
+        "<p>#{block['data']['text']}</p>"
+      else
+        ""
+      end
+    end
+    content_html.join.html_safe
+  end
 end
