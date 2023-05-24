bd1a044bd77211759e989d39802191afdeca4271
Anton Panteleev <friendlyantz@pm.me>
2023-05-24 19:05:01 +1000

    Add EditorJS
    note no {} in `import EditorJS from "@editorjs/editorjs"`

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import EditorJS from "@editorjs/editorjs"
 
 // Connects to data-controller="editor"
 export default class extends Controller {
+  static targets = [ "article_content"];
   connect() {
     console.log("Hello from editor_controller.js")
+
+    this.contentEditor = new EditorJS({
+      holder: this.article_contentTarget,
+    });
   }
 }
